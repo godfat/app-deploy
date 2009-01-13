326ba2e05943dd0a24e259fcedd71877ee23ebee
godfat <godfat@godfat.org>
2009-01-13 15:56:25 +0800

    fixed bugs.... option bugs, and added default options.

--- lib/app-deploy/rack_cluster.rb
+++ lib/app-deploy/rack_cluster.rb
@@ -22,12 +22,12 @@ module RackCluster
       end
     }
 
-    args = [:pid, :log, :user, :group, :chdir].map{ |kind|
-      config_orig.send(:[], kind)
-    }.join("', '")
-
-    init_script = "RackDaemon.daemonize('#{args}')"
-    ruby_opts   = "-r rubygems -r app-deploy/rack_daemon -e \"#{init_script}\""
+    config_orig = { :servers => 1,
+                    :port    => 9292,
+                    :host    => '0.0.0.0',
+                    :log     => 'log/rack-cluster.log',
+                    :pid     => 'tmp/pids/rack-cluster.pid',
+                    :server  => 'mongrel' }.merge(config_orig)
 
     config_orig[:servers].times{ |n|
       config = config_orig.dup
@@ -36,8 +36,15 @@ module RackCluster
       config[:pid] = RackCluster.pid_path(config[:pid], config[:port])
       config[:log] = RackCluster.log_path(config[:log], config[:port])
 
-      yield( config, ruby_opts,
-             rack_opts + " --port #{config[:port]} --pid #{config[:pid]}" )
+      args = [:pid, :log, :user, :group, :chdir].map{ |kind|
+        value = config.send(:[], kind)
+        value ? "'#{value}'" : 'nil'
+      }.join(", ")
+
+      init_script = "RackDaemon.daemonize(#{args})"
+      ruby_opts   = "-r rubygems -r app-deploy/rack_daemon -e \"#{init_script}\""
+
+      yield( config, ruby_opts, rack_opts + " --port #{config[:port]}" )
     }
 
   end
@@ -52,6 +59,7 @@ module RackCluster
   end
 
   def log_path path, port
+    # log should expand path since daemons' working dir is different
     File.expand_path(RackCluster.include_server_number(path, port))
   end
 
