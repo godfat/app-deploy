339b07a125e64776d649d90e5eb0cf06dc79e97d
Lin Jen-Shin <godfat@godfat.org>
2010-05-03 18:35:27 +0800

    utils.rb: don't force people pass arguments

--- lib/app-deploy/utils.rb
+++ lib/app-deploy/utils.rb
@@ -2,7 +2,7 @@
 module AppDeploy
   module_function
   # wrap Rake::Task#invoke for named arguments
-  def invoke task_name, hash
+  def invoke task_name, hash = {}
     task = Rake::Task[task_name]
     args = task.arg_names.map{ |name| hash[name] }
     task.invoke(*args)
